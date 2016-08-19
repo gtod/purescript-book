abe5eb07db3c2481e4fa8ab1463733ac28afb123
Gregory Tod <vlad@gtod.net>
2016-08-19 19:52:57 +1000

    Chapter 3 Exercises

--- chapter3/src/Data/AddressBook.purs
+++ chapter3/src/Data/AddressBook.purs
@@ -3,7 +3,7 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.List (List(..), filter, head, null, nubBy)
 import Data.Maybe (Maybe)
 
 type Address =
@@ -37,3 +37,26 @@ findEntry firstName lastName = head <<< filter filterEntry
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+-- Ex 3.2
+findByAddress :: Address -> AddressBook -> Maybe Entry
+findByAddress address = head <<< filter filterEntry
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.address.street == address.street &&
+                      entry.address.city == address.city &&
+                      entry.address.state == address.state
+
+-- Ex 3.3
+hasName :: String -> String -> AddressBook -> Boolean
+hasName firstName lastName book = not null $ filter filterEntry book
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+-- Ex 3.4
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubBy matching
+  where
+    matching :: Entry -> Entry -> Boolean
+    matching a b = a.firstName == b.firstName && a.lastName == b.lastName
