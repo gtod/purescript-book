97dc3d7e4c2a3b9afcae6ddcf1a2d212e6ee18c5
Gregory Tod <vlad@gtod.net>
2016-08-28 19:39:49 +1000

    fixup

--- chapter4/src/FileOperations.purs
+++ chapter4/src/FileOperations.purs
@@ -32,17 +32,8 @@ smallerPath :: Maybe Path -> Path -> Maybe Path
 smallerPath (Just x) y = if (size x) < (size y) then Just x else Just y
 smallerPath Nothing y = Just y
 
--- You give us a path to look for the smallest file in, but there may not be
--- one so you only get a Maybe Path back...
 smallestFile :: Path -> Maybe Path
--- Again, it's not that we can't make runtime tests but rather that the compiler
--- is satisfied (at compile time) that we have not introduced any type leaks...
--- Note well, smallestFile returns a *function*, it doesn't *do anything* (yet).
 smallestFile = foldl smallerPath Nothing <<< onlyFiles
--- contrast with:
--- smallestFile path = foldl smallerPath Nothing (onlyFiles path)
--- this is written by someone who still wants his functions to *do things* rather
--- than just be pieces of a puzzle to be asembled and (finally) used elsewhere
 
 -- Ex 4.17.3
 -- This works now but is it sort of buggy because of what happens when we get
