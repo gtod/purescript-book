9cf1fc0aa363f6cb4567632623f6f647d13f404c
Gregory Tod <vlad@gtod.net>
2016-08-28 19:35:29 +1000

    Better version of 4.17.3

--- chapter4/src/FileOperations.purs
+++ chapter4/src/FileOperations.purs
@@ -2,9 +2,9 @@ module FileOperations where
 
 import Prelude
 import Control.MonadZero (guard)
-import Data.Array (null, filter, concatMap, (:))
+import Data.Array (concatMap, (:))
 import Data.Foldable (foldl)
-import Data.Maybe (Maybe(Nothing, Just))
+import Data.Maybe (isJust, Maybe(Nothing, Just))
 import Data.Path (isDirectory, size, Path, ls, filename, root)
 
 allFiles :: Path -> Array Path
@@ -45,67 +45,18 @@ smallestFile = foldl smallerPath Nothing <<< onlyFiles
 -- than just be pieces of a puzzle to be asembled and (finally) used elsewhere
 
 -- Ex 4.17.3
-{-
-We have a string and want to match it to a filename
-We must return the directory in which it sits (if any).
-
-Start at the given root node
-Check each of the contained paths (ie ls node)
-  If file
-    If match DONE, returning current path
-  Else
-    recurse
-
-Do the recusion to traverse the tree, but only for directories
-
-
--}
-
-
-
---map (\path -> if isDirectory path then whereIs' path else (if (filename path) == name then path else "")
-
-
-whereIs :: String -> Array (Array Path)
-whereIs name = filter (\x -> not (null x)) (findFile root root)
+-- This works now but is it sort of buggy because of what happens when we get
+-- multiple matches?
+whereIs :: String -> Maybe Path
+whereIs name = foldl (\b a -> if isJust a then a else b) Nothing $ findFile root root
   where
-    matchesName :: Path  -> Boolean
+    matchesName :: Path -> Boolean
     matchesName path = name == filename path
 
-    match :: Path -> Path -> Array Path
-    match path parent = if (matchesName path) then [parent] else []
+    tryMatch :: Path -> Path -> Maybe Path
+    tryMatch path parent = if (matchesName path) then Just parent else Nothing
 
-    findFile :: Path -> Path -> Array (Array Path)
-    findFile node parent = (match node parent) : do
+    findFile :: Path -> Path -> Array (Maybe Path)
+    findFile node parent = (tryMatch node parent) : do
      path <- ls node
      findFile path node
-
--- From https://github.com/ChrisCoffey/ps-by-example/blob/master/src/chapter4/FileSystem.purs
--- whereIs :: String -> Maybe Path
--- whereIs term = let
---     match = do
---         dir <- allDirs
---         matching <- dirMatch dir
---         pure matching
---     in case match of
---         ([dir]) -> Just dir
---         _       -> Nothing
---     where
---         allDirs = filter isDirectory $ allFiles root
---         dirMatch dir =
---             do child <- ls dir
---                guard $ (filename child) == (filename dir <> term)
---                pure dir
-
-
-{-
-KEY POINT: If you can't write this code simply because you do not understand the
-fundamentals of the lnaguage well enough, then FIX that and get on with it.
-
-On the other hand, if the langauge is truly obtuse, do something else.
-
-Might be worth trying to write this three different ways in Common Lisp to compare...
-
-Then blg about that...
-
--}
